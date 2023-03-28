dbaf36305c38fa57a60a1beffe332924200b0953
David Khurtsidze <raingts@mail.ru>
2023-03-28 14:08:04 +0300

    add: news portal project

---
+++ news_portal/news/models.py
@@ -0,0 +1,71 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Sum
+
+from news.resources import *
+
+
+class Author(models.Model):
+    author_user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating_user = models.IntegerField(default=0)
+
+    def update_rating(self):
+        rating_posts_author = Post.objects.filter(author_post=self).aggregate(Sum('rating_post')).get(
+            'rating_post__sum') * 3
+        rating_comments_author = Comment.objects.filter(user_comment=self.author_user).aggregate(
+            Sum('rating_comment')).get('rating_comment__sum')
+        rating_comments_posts = Comment.objects.filter(post_comment__author_post=self.id).aggregate(
+            Sum('rating_comment')).get('rating_comment__sum')
+
+        self.rating_user = rating_posts_author + rating_comments_author + rating_comments_posts
+        print(self.rating_user)
+        self.save()
+
+
+class Category(models.Model):
+    title_category = models.CharField(max_length=255, unique=True)
+
+
+class Post(models.Model):
+    title_post = models.CharField(max_length=255)
+    text_post = models.TextField()
+    author_post = models.ForeignKey(Author, on_delete=models.CASCADE)
+    choice_post = models.CharField(max_length=7, choices=CATEGORY)
+    date_post = models.DateTimeField(auto_now_add=True)
+    categories_post = models.ManyToManyField(Category, through='PostCategory')
+    rating_post = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating_post += 1
+        self.save()
+
+    def dislike(self):
+        self.rating_post -= 1
+        self.save()
+
+    def preview(self):
+        if len(self.text_post) <= 124:
+            return self.text_post
+        else:
+            return self.text_post[0:124] + '...'
+
+
+class PostCategory(models.Model):
+    post_category = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category_post = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post_comment = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_comment = models.ForeignKey(User, on_delete=models.CASCADE)
+    text_comment = models.TextField(default='')
+    date_comment = models.DateTimeField(auto_now_add=True)
+    rating_comment = models.IntegerField(default=0)
+
+    def like(self):
+        self.rating_comment += 1
+        self.save()
+
+    def dislike(self):
+        self.rating_comment -= 1
+        self.save()
