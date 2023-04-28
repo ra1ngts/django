3f206ccd0cc61775500c762d35291cf82daa1775
David Khurtsidze <raingts@mail.ru>
2023-04-28 21:48:08 +0300

    fix: fixed a bug in the template

--- news_portal/news/management/commands/runapscheduler.py
+++ news_portal/news/management/commands/runapscheduler.py
@@ -56,7 +56,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             my_job,
-            trigger=CronTrigger(day_of_week="mon", hour="00", minute="00"),
+            trigger=CronTrigger(day_of_week="mon", hour="00", minute="00"),  # day_of_week="mon", hour="00", minute="00"
             id="my_job",  # The `id` assigned to each job MUST be unique
             max_instances=1,
             replace_existing=True,
