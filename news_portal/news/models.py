045785f477e746ec12aa7415e49065b6bb14f4ba
David Khurtsidze <raingts@mail.ru>
2023-05-22 19:57:32 +0300

    fix: correct work like and dislike as well as edits in the template

--- news_portal/news/models.py
+++ news_portal/news/models.py
@@ -5,7 +5,7 @@ from django.db.models import Sum
 from django.db.models.functions import Coalesce
 from django.urls import reverse
 
-from news.resources import *
+from news.resources import CATEGORY
 
 
 class Author(models.Model):
