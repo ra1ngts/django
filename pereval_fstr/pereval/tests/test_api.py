484445abae09d96fc858e9de97759ee580d0533b
David Khurtsidze <raingts@mail.ru>
2023-07-24 21:16:39 +0300

    fix: fixed api test methods

--- pereval_fstr/pereval/tests/test_api.py
+++ pereval_fstr/pereval/tests/test_api.py
@@ -6,7 +6,7 @@ from pereval.models import Pereval, Coords, Users, Images
 from pereval.serializers import PerevalSerializer, PerevalDetailSerializer
 
 
-class SubmitDataAPITest(APITestCase):
+class SubmitDataGetAPITests(APITestCase):
     def setUp(self):
         self.pereval_1 = Pereval.objects.create(beauty_title='Пер.',
                                                 title='Перевал Pas de Chevres',
@@ -52,16 +52,52 @@ class SubmitDataAPITest(APITestCase):
                                                                              image_3=''))
 
     def test_get_submitData(self):
-        url = reverse('submitData')
-        response = self.client.get(url)
+        response = self.client.get(reverse('submitData'))
         serializer_data = PerevalSerializer([self.pereval_1, self.pereval_2], many=True).data
         self.assertEquals(serializer_data, response.data['results'], msg='Ошибка: Данные не соответствуют')
         self.assertEquals(len(serializer_data), 2, msg='Ошибка: Длина списка не соответствует')
         self.assertEquals(status.HTTP_200_OK, response.status_code, msg='Ошибка: Статус код не соответствует')
 
     def test_get_submitDetailData(self):
-        url = reverse('submitDetailData', args=(self.pereval_1.id,))
-        response = self.client.get(url)
+        response = self.client.get(reverse('submitDetailData', args=(self.pereval_1.id,)))
         serializer_data = PerevalDetailSerializer(self.pereval_1).data
         self.assertEquals(serializer_data, response.data, msg='Ошибка: Данные не соответствуют')
         self.assertEquals(status.HTTP_200_OK, response.status_code, msg='Ошибка: Статус код не соответствует')
+
+
+class SubmitDataPostAPITests(APITestCase):
+    def setUp(self):
+        self.pereval_3 = {
+            'beauty_title': 'Test beauty_title',
+            'title': 'Test title',
+            'other_titles': 'Test other_titles',
+            'connect': 'Test connect',
+            'add_time': '2023-07-20 16:30:32.859243+00',
+            'status': 'new',
+            'level': 'winter-1A',
+            'coordinates': {'latitude': 1.0,
+                            'longitude': 2.0,
+                            'height': 3
+                            },
+            'user': {'first_name': 'Test first name',
+                     'last_name': 'Test last name',
+                     'patronymic': 'Test patronomic',
+                     'email': 'test@example.com',
+                     'phone': '+70123456789'
+                     },
+            'images': {'title_1': 'Test title-1',
+                       'image_1': '',
+                       'title_2': 'Test title-2',
+                       'image_2': '',
+                       'title_3': 'Test title-3',
+                       'image_3': ''
+                       }
+        }
+
+    def test_post_submitData(self):
+        response = self.client.post(reverse('submitData'), self.pereval_3, format='json')
+        self.assertEqual(status.HTTP_200_OK, response.status_code, msg='Ошибка: Статус код не соответствует')
+        self.assertEqual(Pereval.objects.count(), 1, msg='Ошибка: Количество не соответствует')
+        self.assertEqual(Pereval.objects.get().beauty_title, 'Test beauty_title', msg='Ошибка: Данные не соответствуют')
+        self.assertEqual(Users.objects.get().first_name, 'Test first name', msg='Ошибка: Данные не соответствуют')
+        self.assertEqual(Coords.objects.get().latitude, 1.0, msg='Ошибка: Данные не соответствуют')
